6f55c4555cf5e29d24ed660177b61b2762b34a61
MatasA <aleliunasmatas@gmail.com>
2023-02-20 10:55:56 +0200

    eslint sukonfiguruotas

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
   * [Node](https://nodejs.org/en/) v16 or later
   
 ## Project Setup
-  * copy file [.env.example](.env.example) nad rename it to __.env__
+  * copy file [.env.example](.env.example) and rename it to __.env__
   * define constants if file __.env__
   * instal libraries - open terminal in root directory and run command:
   ```bash
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
-import * as dotenv from 'dotenv'
-dotenv.config();
-import express from 'express'
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import express from 'express';
 
-const {SERVER_PORT, SERVER_DOMAIN} = process.env;
+dotenv.config();
+
+const { SERVER_PORT, SERVER_DOMAIN } = process.env;
 
 if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
-  throw new Error('Pleise define constants in .env file')
+  throw new Error('Pleise define constants in .env file');
 }
 
 const server = express();
+server.use(morgan('tiny'));
+
 server.use(express.static('public'));
 
 server.listen(SERVER_PORT, () => {
-  console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`)
-})
+  console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`);
+});
