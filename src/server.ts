64d21ee0e4ff2ce1f3535dd83a52a0776031077e
MatasA <aleliunasmatas@gmail.com>
2023-02-20 14:05:34 +0200

    implemented post and get

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,31 @@ if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
 }
 
 const server = express();
-server.use(morgan('tiny'));
 
+server.use(morgan('tiny'));
 server.use(express.static('public'));
+server.use(express.json());
+
+const movies = [
+  { id: 1, title: 'movie1' },
+  { id: 2, title: 'movie2' },
+  { id: 3, title: 'movie3' },
+  { id: 4, title: 'movie4' },
+];
+
+const apiRouter = express.Router();
+server.use('/api', apiRouter);
+
+apiRouter.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+});
+
+apiRouter.post('/movies', (req, res) => {
+  const { body } = req;
+  const newMovie = { id: 5, title: body.title };
+  movies.push(newMovie);
+  res.status(201).json(newMovie);
+});
 
 server.listen(SERVER_PORT, () => {
   console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`);
