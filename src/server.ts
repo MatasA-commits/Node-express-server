2a160721fb9b9276d6d2f0819c37da1aa726538c
MatasA <aleliunasmatas@gmail.com>
2023-02-23 10:51:26 +0200

    feat: connected to mysql

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import morgan from 'morgan';
 import express from 'express';
+import mysql from 'mysql';
 import config from './config';
 import moviesRouter from './controllers/movies-controller';
+import { connectMySql } from './services/my-sql';
 
 const server = express();
 
 server.use(morgan('tiny'));
 server.use(express.static('public'));
 server.use(express.json());
-
 server.use('/api/movies', moviesRouter);
 
-server.listen(config.server.port, () => {
-  console.log(`server is running on: http://${config.server.domain}:${config.server.port}`);
+connectMySql(() => {
+  server.listen(config.server.port, () => {
+    console.log(`server is running on: http://${config.server.domain}:${config.server.port}`);
+  });
 });
