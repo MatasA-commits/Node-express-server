16507da3c487a4ab2bd9127a36b6073e9ad0b391
MatasA <aleliunasmatas@gmail.com>
2023-02-27 10:04:44 +0200

    feat: created database schema uml diagram

--- src/sql/migration/migration-2023-02-24-11-01-create-movie-tables.sql
+++ src/sql/migration/migration-2023-02-24-11-01-create-movie-tables.sql
@@ -10,7 +10,7 @@ create table movies (
   id int4 unsigned primary key auto_increment,
   title varchar(256) not null,
   mainCharacterId int4 unsigned not null unique,
-  year varchar(5) not null,
+  year varchar(4) not null,
   rating float4 not null,
   createdAt timestamp default current_timestamp,
   updatedAt timestamp default current_timestamp on update current_timestamp,
