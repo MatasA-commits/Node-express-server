747363d2f3d70c7adfb2e590d3fec149692fb69d
MatasA <aleliunasmatas@gmail.com>
2023-03-09 11:46:39 +0200

    feat: implemented update authorization check

--- src/controllers/movies-controller/mutations/update-movie.ts
+++ src/controllers/movies-controller/mutations/update-movie.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from 'express';
-import ErrorService, { ServerSetupError } from '../../../services/error-service';
+import ErrorService, { ServerSetupError, ForbiddenError } from '../../../services/error-service';
 import MoviesModel from '../model';
 import { MovieViewModel, PartialMovieDataValidation } from '../types';
 import partialMovieDataValidationSchema from '../validation-schemas/movie-data-partial-validation-schema';
+import UserModel from '../auth/model/index';
 
 export const updateMovie: RequestHandler<
 { id: string | undefined },
@@ -14,10 +15,14 @@ PartialMovieDataValidation,
 
   try {
     if (id === undefined) throw new ServerSetupError();
+    if (req.authData === undefined) throw new ServerSetupError();
     const partialMovieData = partialMovieDataValidationSchema.validateSync(
       req.body,
       { abortEarly: false },
     );
+    const user = await UserModel.getUserByEmail(req.authData.email);
+    const movie = await MoviesModel.getMovie(id);
+    if (user.role !== 'ADMIN' && user.id !== movie.owner.id) throw new ForbiddenError();
 
     const updatedMovie = await MoviesModel.updateMovie(id, partialMovieData);
     res.status(200).json(updatedMovie);
